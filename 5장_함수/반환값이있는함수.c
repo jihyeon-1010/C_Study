1774a31ab82ce0b43e763e686736ade108b79f5e
leejihyeon <l91229122leejh@gmail.com>
2024-07-07 13:47:55 +0900

    반환값이 있는 함수

---
+++ 5장_함수/반환값이있는함수.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+// 함수 선언
+int function_with_return();
+void p(int num);
+
+int main(void) {
+    int ret = function_with_return();
+    p(ret);
+    
+    return 0;
+}
+
+// 함수 정의
+int function_with_return() {
+    printf("반환값이 있는 함수입니다.\n");
+    return 10; 
+}
+
+void p(int num) {
+    printf("num은 %d입니다.\n", num);
+}
