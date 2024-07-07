eb5ca1538f77f718d556e7623679ce95154e7c31
leejihyeon <l91229122leejh@gmail.com>
2024-07-07 13:42:41 +0900

    break문

---
+++ 4장_조건문/break.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+    int i;
+    for (i = 1; i <= 30; i++) {
+        if (i >= 6) {
+            printf("나머지 학생은 집에 가세요.\n");
+            break;
+        }   
+        printf("%d번 학생은 조별 발표를 준비하세요.\n", i); 
+    }   
+
+    return 0;
+}
