d36363fdc62f6ab7283699c4901fe21a87861d5d
leejihyeon <l91229122leejh@gmail.com>
2024-07-07 13:38:38 +0900

    셀프체크

---
+++ 3장_반복문/self_check.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+    int i, j;
+    for (i = 5; i > 0; i--) {
+        for (j = i; j > 0; j--) {
+            printf("*");
+        }   
+        printf("\n");
+    }   
+
+    return 0;
+}
