12f9aed4508468435da0216a9c248f6a552dd1b9
leejihyeon <l91229122leejh@gmail.com>
2024-07-07 13:52:19 +0900

    반환값과 전달값이 있는 함수

--- 5장_함수/반환값과전달값이있는함수.c
+++ 5장_함수/반환값과전달값이있는함수.c
@@ -6,7 +6,7 @@ int main(void) {
     int ret = apple(5, 2); 
     printf("사과 5개 중에서 2개를 먹으면 % d개가 남습니다.\n", ret);
     return 0;
-}`
+}
 
 int apple(int total, int ate) {
     printf("전달값과 반환값이 있는 함수입니다.\n");
